fe37980b52aac7db002c5e50216204c5a8b41454
Derek Cormier <derek.m.cormier@gmail.com>
2025-02-27 13:25:58 -0800

    temp: foobar

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -18,6 +18,9 @@ git archive --format=tar --prefix=${PREFIX}/ ${TAG} >$ARCHIVE_TMP
 gzip <$ARCHIVE_TMP >$ARCHIVE
 SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
+# Random extra release file
+echo "foo" > bar.txt
+
 cat <<EOF
 
 ## Release notes
@@ -26,4 +29,6 @@ Foobar
 
 \`\`\`
 
-EOF
+EOF > release_notes.txt
+
+jq --null-input --arg archive "${ARCHIVE}" '{release_archive: $archive, upload_extra_artifacts: ["bar.txt"], release_notes: "release_notes.txt"}'
